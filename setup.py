9893ce482c7500703078056646fb7957384e3f3a
mdraw <mdraw.gh@gmail.com>
2017-01-18 19:01:42 +0100

    Add skimage dependency

--- setup.py
+++ setup.py
@@ -47,6 +47,7 @@ setup(
         'seaborn',
         'scikit-learn<0.20',  # >=0.20 requires change, see todo-note in data.traindata import
         'psutil>=5.0.1',
+        'scikit-image>=0.12.3',
     ],
     extras_require={
         'numba': ['numba>=0.25'],
