70c325cf32e935c7c4b671695dabaf07992df100
mdraw <mdraw.gh@gmail.com>
2017-01-18 18:51:58 +0100

    Add psutil dependency

--- setup.py
+++ setup.py
@@ -46,6 +46,7 @@ setup(
         'pydotplus',
         'seaborn',
         'scikit-learn<0.20',  # >=0.20 requires change, see todo-note in data.traindata import
+        'psutil>=5.0.1',
     ],
     extras_require={
         'numba': ['numba>=0.25'],
