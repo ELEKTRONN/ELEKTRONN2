638cffa2d1185853c79e10b36b7b789cde3924d2
mdraw <mdraw.gh@gmail.com>
2017-02-20 21:27:43 +0100

    Tune and clean up MNIST example

--- examples/mnist.py
+++ examples/mnist.py
@@ -3,17 +3,19 @@
 # Copyright (c) 2016 Philipp J. Schubert
 # All rights reserved
 save_path = '~/elektronn2_examples/'
-save_name = "mnist"
-data_class = 'MNISTData' # <String>: Name of Data Class in TrainData or <tuple>: (path_to_file, class_name)
+save_name = 'MNIST'
+data_class = 'MNISTData' # <String>: Name of the data class in
+                         # ``elektronn2.data.traindata`` (as used here) or
+                         # <tuple>: (path_to_file, class_name)
 background_processes = 2
 
-n_steps = 300000
-max_runtime = 4 * 24 * 3600 # in seconds
+n_steps = 30000
+max_runtime = 10 * 60 # in seconds
 history_freq = 500
 monitor_batch_size = 20
 optimiser = 'Adam'
 data_batch_args = {}
-data_init_kwargs = {}#'shift_augment': False}
+data_init_kwargs = {}
 optimiser_params = dict(lr=2e-4, mom=0.9, beta2=0.99, wd=0.5e-3)
 batch_size = 20
 
@@ -24,13 +26,13 @@ def create_model():
     in_sh = (None,1,26,26)
     inp = neuromancer.Input(in_sh, 'b,f,y,x', name='raw')
 
-    out = neuromancer.Conv(inp, 12,  (3,3), (2,2), activation_func=act, batch_normalisation = 'train')
-    out = neuromancer.Conv(out, 36,  (3,3), (2,2), activation_func=act, batch_normalisation = 'train')
-    out = neuromancer.Conv(out, 64,  (3,3), (1,1), activation_func=act, batch_normalisation = 'train')
+    out = neuromancer.Conv(inp, 12, (3,3), (2,2), activation_func=act, batch_normalisation='train')
+    out = neuromancer.Conv(out, 36, (3,3), (2,2), activation_func=act, batch_normalisation='train')
+    out = neuromancer.Conv(out, 64, (3,3), (1,1), activation_func=act, batch_normalisation='train')
     out = neuromancer.Perceptron(out, 200, flatten=True)
     out = neuromancer.Perceptron(out, 10, activation_func='lin')
     out = neuromancer.Softmax(out)
-    target   = neuromancer.Input_like(out, override_f=1, name='target')
+    target = neuromancer.Input_like(out, override_f=1, name='target')
     loss  = neuromancer.MultinoulliNLL(out, target, name='nll_', target_is_sparse=True)
     # Objective
     loss = neuromancer.AggregateLoss(loss)
@@ -38,22 +40,24 @@ def create_model():
     errors = neuromancer.Errors(out, target, target_is_sparse=True)
 
     model = neuromancer.model_manager.getmodel()
-    model.designate_nodes(input_node=inp, target_node=target, loss_node=loss,
-                                  prediction_node=out,
-                                  prediction_ext=[loss, errors, out])
+    model.designate_nodes(
+        input_node=inp,
+        target_node=target,
+        loss_node=loss,
+        prediction_node=out,
+        prediction_ext=[loss, errors, out])
     return model
 
 if __name__ == "__main__":
     import traceback
     model = create_model()
 
-
     try:
         model.test_run_prediction()
     except Exception as e:
         traceback.print_exc()
-        print("Test run failed. In case your GPU ran out of memory the \
-               principal setup might still be working")
+        print('Test run failed.\nIn case your GPU ran out of memory, the '
+              'principal setup might still be working')
 
     try:
         from elektronn2.utils.d3viz import visualise_model
@@ -63,4 +67,5 @@ if __name__ == "__main__":
         webbrowser.open('model-graph.html')
     except Exception as e:
         traceback.print_exc()
-        print("Could not print model model graph. Is pydot/graphviz properly installed?")
+        print('Could not print model model graph.\n'
+              'Are pydot and graphviz properly installed?')
