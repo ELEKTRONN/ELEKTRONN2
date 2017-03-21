857739a034f9dd858fd9deba45fc5be8d5e8d23c
mdraw <mdraw.gh@gmail.com>
2017-03-22 00:03:50 +0100

    Docs/Sphinx build: Fix remaining import errors
    Fixing complicated build dependency problems by using
    my "fake" libraries
    - https://github.com/mdraw/e2fakenumba
    - https://github.com/mdraw/knossos_utils
    
    Building on readthedocs.io with CPython 2.7 should
    work now and enable apidoc output of the missing modules
    (elektronn2.data.*) that were previously unavailable
    because they depend on numba and knossos_utils

--- docs/conf.py
+++ docs/conf.py
@@ -21,22 +21,6 @@
 # import sys
 # sys.path.insert(0, os.path.abspath('.'))
 
-# -- Mock problematic libraries that are unnecessary for doc building -----
-
-import sys
-from unittest.mock import MagicMock
-
-class Mock(MagicMock):
-    @classmethod
-    def __getattr__(cls, name):
-            return MagicMock()
-
-MOCK_MODULES = [
-    'knossos_utils',
-]
-sys.modules.update((mod_name, Mock()) for mod_name in MOCK_MODULES)
-
-
 # -- General configuration ------------------------------------------------
 
 # If your documentation needs a minimal Sphinx version, state it here.
