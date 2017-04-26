245d7cde76a6a5484df63b4af570b98bb7465c34
mdraw <mdraw.gh@gmail.com>
2017-04-26 18:59:13 +0200

    Force theano<0.10 due to breaking changes

--- setup.py
+++ setup.py
@@ -50,7 +50,7 @@ setup(
         'scipy>=0.14',
         'matplotlib>=1.4',
         'h5py>=2.2',
-        'theano>=0.7',
+        'theano>=0.7,<0.10',  # ELEKTRONN2 relies on the old Theano backend that has been removed now
         'future>=0.15',
         'tqdm>=4.5',
         'colorlog>=2.7',
