ELEKTRONN/ELEKTRONN2

commit 684b0e7a397b3c49d3ad4d65aab329a7774fc71f

gpuarray: Remove last use of cuda-specific code
This breaks the device variable since it currently isn't clear
what device is being used.

elektronn2/training/trainutils.py
@@ -21,8 +21,6 @@ from os.path import abspath, dirname
 import numpy as np
 from matplotlib import pyplot as plt
 from scipy.integrate import cumtrapz as integrate
-import theano
-import theano.sandbox.cuda
 
 from .. import utils
 from ..config import config, change_logging_file
@@ -513,9 +511,8 @@ class ExperimentConfig(object):
         now = datetime.datetime.today().isoformat()
         device = 'CPU'
         try:
-            if theano.sandbox.cuda.cuda_enabled:
-                device = 'GPU {}'.format(
-                    theano.sandbox.cuda.active_device_number())
+            device = 'unknown (libgpuarray)'  # TODO: How to find the device name with gpuarray?
+            # device = 'GPU {}'.format(theano.sandbox.cuda.active_device_number())
         except:
             pass
         logger.info('Running on {}@{}, using {}. Start time: {}'.format(

commit 97c7f37db2a47eb709f4cc0815e984709dbb7130

gpu: Better GPU handling
- Use config.device to actually store device name on startup (e.g. 'cpu' or 'cuda0'...)
- Properly log device on init of ExperimentConfig (fixing regression in 684b0e7)
- gpu.initgpu() now returns the name of the device that has been initialized
- Make default gpu device prefix configurable in gpu.get_free_gpu() and gpu.initgpu()

elektronn2/config.py
@@ -65,7 +65,7 @@ class DefaultConfig(object):
         self.save_path = os.path.abspath(os.path.expanduser("~/CNN_Training/"))
         self.plot_on = True  # <Bool>: whether to create plots of the errors etc.
         self.print_status = True  # <Bool>: whether to print Training status to std.out
-        self.device = None  # None/int/'auto' (use .theanorc value) or int (use gpu<i>)
+        self.device = None  # None|'auto'|'none'|'cuda<int>' (use .theanorc value) or int (use GPU <i>)
         self.param_save_h = 1.0  # hours: frequency to save a permanent parameter snapshot
         self.param_save_it = 10000
         self.initial_prev_h = 1.0  # hours: time after which first preview is made
@@ -93,15 +93,15 @@ class DefaultConfig(object):
         # Init GPU stuff
         if self.device or self.device==0:
             if self.device=='auto':
-                gpu_num = gpu.get_free_gpu(wait=2.0)
-                if gpu_num < 0:
+                _device = gpu.get_free_gpu(wait=2.0)
+                if _device < 0:
                     raise RuntimeError("Could not find free GPU.")
 
             else:
-                assert isinstance(self.device, int)
-                gpu_num = self.device
+                # assert isinstance(self.device, int)
+                _device = self.device
 
-            gpu.initgpu(gpu_num)
+            gpu.initgpu(_device)
 
         change_logging_file(logger, self.save_path, file_name='elektronn2.log')
 

elektronn2/training/trainutils.py
@@ -509,14 +509,8 @@ class ExperimentConfig(object):
 
         host_name = socket.gethostname()
         now = datetime.datetime.today().isoformat()
-        device = 'CPU'
-        try:
-            device = 'unknown (libgpuarray)'  # TODO: How to find the device name with gpuarray?
-            # device = 'GPU {}'.format(theano.sandbox.cuda.active_device_number())
-        except:
-            pass
-        logger.info('Running on {}@{}, using {}. Start time: {}'.format(
-            user_name, host_name, device, now))
+        logger.info('Running on {}@{}, using device {}. Start time: {}'.format(
+            user_name, host_name, config.device, now))
 
     def read_user_config(self):
         logger.info("Reading exp_config-file %s" % (self.exp_file,))

elektronn2/utils/gpu.py
@@ -13,7 +13,7 @@ import subprocess
 import time
 
 
-def initgpu(gpu):
+def initgpu(gpu, prefix='cuda'):
     if gpu is None:
         gpu = 'none'
     no_gpu = ['none', 'None']
@@ -26,15 +26,17 @@ def initgpu(gpu):
         print("Automatically assigning free GPU %s" % (gpu,))
 
     if gpu in no_gpu and gpu != '0':
-        pass
+        return 'cpu'
     else:
         try:
-            if False or gpu.isdigit():
-                gpu = 'cuda{}'.format(gpu)
+            if gpu.isdigit():  # If int, prepend prefix
+                gpu = '{}{}'.format(prefix, gpu)
                 theano.gpuarray.use(gpu)
             else:
                 theano.gpuarray.use(gpu)
             print("Initialising GPU to %s" % gpu)
+            return gpu
+
         except:
             sys.excepthook(*sys.exc_info())
             raise RuntimeError("Failed to init GPU {}. Aborting...".format(gpu))
@@ -59,9 +61,9 @@ def _check_if_gpu_is_free(nb_gpu):
               'Please make sure CUDA is available on your machine.\n')
         raise e
     if b"Process ID" in process_output and b"Used GPU Memory" in process_output:
-        return 0
+        return False
     else:
-        return 1
+        return True
 
 
 def _get_number_gpus():
@@ -82,13 +84,13 @@ def _get_number_gpus():
     return nb_gpus
 
 
-def get_free_gpu(wait=0, nb_gpus=-1):
+def get_free_gpu(wait=0, nb_gpus=-1, prefix='cuda'):
     if nb_gpus==-1:
         nb_gpus = _get_number_gpus()
     while True:
         for nb_gpu in range(nb_gpus):
-            if _check_if_gpu_is_free(nb_gpu)==1:
-                return nb_gpu
+            if _check_if_gpu_is_free(nb_gpu):
+                return '{}{}'.format(prefix, nb_gpu)
         if wait > 0:
             time.sleep(2)
         else:

scripts/elektronn2-train
@@ -72,11 +72,14 @@ from elektronn2.config import config
 
 if config.device is None:
     from elektronn2.utils.gpu import initgpu
-    initgpu(gpu)
+    config.device = initgpu(gpu)
 else:
     if gpu:
         print("Cannot init gpu to %s because there is already the value %s "
               "in ~/.elektronn2rc." %(gpu, config.device))
+    else:
+        config.device = 'cpu'
+
 
 from elektronn2.training import trainer
 from elektronn2.training import trainutils