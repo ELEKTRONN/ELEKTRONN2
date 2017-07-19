167396206c8440509b128167ad113e0e21e5d708
mdraw <mdraw.gh@gmail.com>
2017-07-19 19:31:10 +0200

    docs: Fix table CSS, text wraps normally now
    CSS override by http://rackerlabs.github.io/docs-rackspace/tools/rtd-tables.html

--- docs/conf.py
+++ docs/conf.py
@@ -111,6 +111,12 @@ html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
 
+html_context = {
+    'css_files': [
+        '_static/table_fix.css',
+    ],
+}
+
 
 # -- Options for HTMLHelp output ------------------------------------------
 
