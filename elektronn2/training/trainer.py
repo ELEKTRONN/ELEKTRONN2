81483b74fd4bc1a23320353738a95f47eb2f0a24
mdraw <mdraw.gh@gmail.com>
2017-08-20 21:42:21 +0200

    previews: Support (path, key) tuples as data path
    Making it easier to reuse existing hdf5 files with multiple
    data sets.

--- elektronn2/training/trainer.py
+++ elektronn2/training/trainer.py
@@ -117,7 +117,11 @@ class Trainer(object):
 
     def _load_preview_data(self):
         if self.exp_config.preview_data_path is not None:
-            data = utils.h5load(self.exp_config.preview_data_path)
+            try:  # Tuple of path and data set key
+                path, key = self.exp_config.preview_data_path
+                data = utils.h5load(path, key)
+            except (TypeError, ValueError):  # Use default key
+                data = utils.h5load(self.exp_config.preview_data_path)
             if not (isinstance(data, list) or  isinstance(data, (tuple, list))):
                 data = [data,]
             data = [d.astype(floatX)/d.max() for d in data]
