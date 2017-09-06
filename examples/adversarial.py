- fixed bug in loss calculation loss calculation during trainee network training - added FlipNode; if internal attribute do_flip is set, it will invert its binary input (1- input); used to increase gradient during trainee network training - small architecture changes - also plotting adversarial vs. trainee loss now (debug_output) - added variable to scale adversarial learning rate compared to trainee LR - added AdvTarget node which creates random indices (betw. 0 and 1) to feed corresponding input (0: trainee, 1: ground truth) as input to adversarial network - added AdvMerge node which merges inputs (both of size b; trainee output and ground truth) to single output of size b containing samples corresponding to third input (output of AdvTarget node)# Please enter the commit message for your changes. Lines starting

examples/adversarial.py
@@ -14,25 +14,27 @@ trainee_path = os.path.expanduser("~/devel/ELEKTRONN2/examples/unet3d_litelite.p
 trainee_dict = dict()
 exec (compile(open(trainee_path).read(), trainee_path, 'exec'), {},
       trainee_dict)
-alpha = 0.2 # factor for adversarial loss term
-adv_class_w = [[0, 1], [1, 1]]
-mixing_w = [[alpha, 1], [1, 0]]
+# training is alternated between training trainee and adversarial network
+# TODO: define #steps for each network individually
+p = [.5, .5]  # probability to present ground truth to adversarial network; first entry while training trainee; second entry while training adversarial
+adv_class_w = [[0, 1], [1, 1]]  # class weights for adversarial loss term, 0: trainee output 1: ground truth; first entry while training trainee network, i.e. here onyl the class 1 (ground truth)
+mixing_w = [[0.5, 1], [1, 0]]  # mixing weights for adversarial and trainee loss; instead of training -lambda l_bce(a(x, s(x)), 0), we use +lambda l_bce(a(x, s(x)), 1) -> stronger gradient (see Goodfellow et al. 2014)
 # permut_steps: number of steps after which training is switched
 # (0-9: segmentor, 10-19: adversarial, 20:29: segmentor, ...)
 # mixing weights
 network_arch = {"adversarial": {"permut_steps": 10, 'mixing_weights': mixing_w,
-    'class_weights': adv_class_w}}
+    'class_weights': adv_class_w, "target_p": p, "lr_fact": 0.1}}
 initial_prev_h = 1.0  # hours: time after which the first preview is made
 prev_save_h = 1.0  # hours: time interval between planned previews.
 data_class = trainee_dict["data_class"]
 background_processes = 2
-h5_fnames = get_filepaths_from_dir('/wholebrain/scratch/j0126/barrier_gt_phil/', ending=".h5")
+h5_fnames = get_filepaths_from_dir('/wholebrain/scratch/j0126/barrier_gt_phil/', ending="rawbarr-zyx.h5")
 data_init_kwargs = {
-    'zxy': False,
-    'make_l_unique' : True,
+    'zxy': True,
+    'make_l_unique' : False,
     'd_path' : '/wholebrain/scratch/j0126/barrier_gt_phil/',
     'l_path': '/wholebrain/scratch/j0126/barrier_gt_phil/',
-    'd_files': [(os.path.split(fname)[1], 'em_raw') for fname in h5_fnames],
+    'd_files': [(os.path.split(fname)[1], 'raW') for fname in h5_fnames],
     'l_files': [(os.path.split(fname)[1], 'labels') for fname in h5_fnames],
     'aniso_factor': 2,
     'valid_cubes': [2],
@@ -47,15 +49,16 @@ data_batch_args = {
 }
 n_steps = 1500000
 max_runtime = 4 * 24 * 3600  # in seconds
-history_freq = 800
+history_freq = 400
 monitor_batch_size = 5
-optimiser = 'SGD'
+optimiser = 'Adam'
 dr = 0.01  # dropout
 act = 'relu'
 optimiser_params = {
-    'lr': 0.0005,
+    'lr': 0.0001,
     'mom': 0.9,
-    'wd': 0.5e-4
+    'wd': 0.5e-4,
+    'beta2': 0.999
 }
 schedules = {
     'lr': {'dec': 0.995},  # decay (multiply) lr by this factor every 1000 steps
@@ -69,37 +72,39 @@ def create_model():
     trainee_gt = trainee.target_node
     trainee_out = trainee.prediction_node
     trainee_loss = trainee.loss_node
-    adv_input, adv_target = nm.advmerge(trainee_out, trainee_gt)
+    adv_target = nm.AdvTarget([trainee_out, trainee_gt])
+    adv_input = nm.AdvMerge([trainee_out, trainee_gt, adv_target])
 
     # raw data
     diff = np.array(inp.shape.spatial_shape, dtype=np.int32)-np.array(trainee_out.shape.spatial_shape, dtype=np.int32)
     assert not np.any(diff % 2)
     raw_inp = nm.Crop(inp, list((diff // 2)))
-    conv0  = nm.Conv(raw_inp,  5,  (2,3,3), activation_func=act, dropout_rate=dr, name="conv_adv")
-    conv1  = nm.Conv(conv0, 10,  (2,3,3), activation_func=act, dropout_rate=dr, name="conv_adv")
+    conv0  = nm.Conv(raw_inp,  15,  (2,3,3), activation_func=act, dropout_rate=dr, name="conv_adv")
+    conv1  = nm.Conv(conv0, 20,  (2,3,3), activation_func=act, dropout_rate=dr, name="conv_adv")
     down0  = nm.Pool(conv1, (1,2,2), mode='max')  # mid res
-    conv2  = nm.Conv(down0, 10,  (2,3,3), activation_func=act, dropout_rate=dr, name="conv_adv")
-    conv3  = nm.Conv(conv2, 15,  (2,3,3), activation_func=act, dropout_rate=dr, name="conv_adv")
+    conv2  = nm.Conv(down0, 20,  (2,3,3), activation_func=act, dropout_rate=dr, name="conv_adv")
+    conv3  = nm.Conv(conv2, 25,  (2,3,3), activation_func=act, dropout_rate=dr, name="conv_adv")
     # Merging low-res with mid-res features
-    mrg1   = nm.UpConvMerge(conv1, conv3, 20, name="upconv_adv")
-    mconv2 = nm.Conv(mrg1, 15,  (2,3,3), activation_func=act, dropout_rate=dr, name="conv_adv")
-    raw_out = nm.Conv(mconv2, 15,  (2,3,3), activation_func=act, dropout_rate=dr, name="conv_adv")
+    mrg1   = nm.UpConvMerge(conv1, conv3, 30, name="upconv_adv")
+    mconv2 = nm.Conv(mrg1, 25,  (2,3,3), activation_func=act, dropout_rate=dr, name="conv_adv")
+    raw_out = nm.Conv(mconv2, 25,  (2,3,3), activation_func=act, dropout_rate=dr, name="conv_adv")
 
     # segmentation
-    conv0  = nm.Conv(adv_input, 5, (2,3,3), activation_func=act, dropout_rate=dr, name="conv_adv")
-    conv1  = nm.Conv(conv0, 10, (2,3,3), activation_func=act, dropout_rate=dr, name="conv_adv")
+    conv0  = nm.Conv(adv_input, 15, (2,3,3), activation_func=act, dropout_rate=dr, name="conv_adv")
+    conv1  = nm.Conv(conv0, 20, (2,3,3), activation_func=act, dropout_rate=dr, name="conv_adv")
     down0  = nm.Pool(conv1, (1,2,2), mode='max')  # mid res
-    conv2  = nm.Conv(down0, 10, (2,3,3), activation_func=act, dropout_rate=dr, name="conv_adv")
-    conv3  = nm.Conv(conv2, 15, (2,3,3), activation_func=act, dropout_rate=dr, name="conv_adv")
+    conv2  = nm.Conv(down0, 20, (2,3,3), activation_func=act, dropout_rate=dr, name="conv_adv")
+    conv3  = nm.Conv(conv2, 25, (2,3,3), activation_func=act, dropout_rate=dr, name="conv_adv")
     # Merging low-res with mid-res features
-    mrg1   = nm.UpConvMerge(conv1, conv3, 20, name="upconv_adv")
-    mconv2 = nm.Conv(mrg1, 15,  (2,3,3), activation_func=act, dropout_rate=dr, name="conv_adv")
-    seg_out = nm.Conv(mconv2, 15,  (2,3,3), activation_func=act, dropout_rate=dr, name="conv_adv")
+    mrg1   = nm.UpConvMerge(conv1, conv3, 30, name="upconv_adv")
+    mconv2 = nm.Conv(mrg1, 25,  (2,3,3), activation_func=act, dropout_rate=dr, name="conv_adv")
+    seg_out = nm.Conv(mconv2, 25,  (2,3,3), activation_func=act, dropout_rate=dr, name="conv_adv")
 
     out = nm.Concat([raw_out, seg_out], axis="f")
-    out = nm.Conv(out, 20, (2, 5, 5), pool_shape=(1, 2, 2), activation_func=act, dropout_rate=dr, name="conv_adv")
+    out = nm.Conv(out, 30, (2, 3, 3), pool_shape=(1, 1, 1), activation_func=act, dropout_rate=dr, name="conv_adv")
     out = nm.Conv(out, 40, (2, 3, 3), pool_shape=(1, 2, 2), activation_func=act, dropout_rate=dr, name="conv_adv")
-    out = nm.Conv(out, 60, (2, 2, 2), pool_shape=(1, 2, 2), activation_func=act, dropout_rate=dr, name="conv_adv")
+    out = nm.Conv(out, 60, (2, 3, 3), pool_shape=(1, 2, 2), activation_func=act, dropout_rate=dr, name="conv_adv")
+    out = nm.Conv(out, 2, (1, 3, 3), pool_shape=(1, 1, 1), activation_func=act, dropout_rate=dr, name="conv_adv")
     dec  = nm.Perceptron(out, 2, flatten=True, activation_func='lin', name="perc_adv")
     adv_out = nm.Softmax(dec)
 
@@ -108,8 +113,12 @@ def create_model():
     # for the adv. prediction loss when training the trainee
     # if training the adv. network only the binary cross-entropy of the adv.
     # prediction is backpropagated.
-    adv_loss = nm.MultinoulliNLL(adv_out, adv_target, target_is_sparse=True, name='nll_adversarial', class_weights=None)
-    loss = nm.AggregateLoss([adv_loss, trainee_loss] , mixing_weights=None, name='loss_adversarial')
+    # flip node: flips target if training the trainee network, in order to have stronger gradients (see above, or 'Generative adversarial nets' from Goodfellow et al. 2014)
+    # goal: maximize probability of adv. net that it predicts an output of the trainee network as ground truth map
+    flipped_adv_target = nm.FlipNode(adv_target)
+    adv_loss = nm.MultinoulliNLL(adv_out, flipped_adv_target, target_is_sparse=True, name='nll_adversarial', class_weights=None)
+    adv_loss = nm.AggregateLoss([adv_loss], name='loss_adversarial')
+    loss = nm.AggregateLoss([adv_loss, trainee_loss] , mixing_weights=None, name='loss_total')
 
     model = nm.model_manager.getmodel()
     model.designate_nodes(
@@ -118,6 +127,7 @@ def create_model():
         loss_node=loss,
         prediction_node=trainee.prediction_node,
         prediction_ext=trainee.prediction_ext,
+        debug_outputs=[adv_loss, trainee_loss, adv_target]
     )
     return model
 