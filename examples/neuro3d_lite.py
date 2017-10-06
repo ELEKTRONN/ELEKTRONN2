86fea4cbb924c3f40b7371afe218b5b5e05cf93e
mdraw <mdraw.gh@gmail.com>
2017-10-06 16:01:51 +0200

    examples: Increase n_steps to 1 million
    To make sure we don't stop too early in training.

--- examples/neuro3d_lite.py
+++ examples/neuro3d_lite.py
@@ -26,7 +26,7 @@ data_batch_args = {
         'perspective': True
     }
 }
-n_steps = 150000
+n_steps = 1000000
 max_runtime = 7 * 24 * 3600 # in seconds
 history_freq = 200
 monitor_batch_size = 30
